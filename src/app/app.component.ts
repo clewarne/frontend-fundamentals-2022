04772a69d1f8894e3ead2297f2bd7f0e4293f580
Christel Lewarne <clewarne@retrorabbit.co.za>
2022-01-13 13:22:55 +0200

    Update firebase setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Analytics, getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAk6OrxcLWYRU2r4cIUCT99tAE76n7pK_A",
   authDomain: "frontendfundamentals-9be86.firebaseapp.com",
@@ -27,9 +24,10 @@ const firebaseConfig = {
 export class AppComponent {
   title = 'chatterbox';
 
+  app: FirebaseApp;
+
   constructor() {
     // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
+    this.app = initializeApp(firebaseConfig);
   }
 }
