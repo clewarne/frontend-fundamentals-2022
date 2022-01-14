465b820a298fe2738ae023de7d1787762e8d3637
Christel Lewarne <clewarne@retrorabbit.co.za>
2022-01-14 07:42:08 +0200

    Setup routing and added bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ChatroomComponent } from './pages/chatroom/chatroom.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'chatroom/:nickname', component: ChatroomComponent },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
